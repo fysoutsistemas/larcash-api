f8cdf818180825d621d807a315619f0007967bac
laudelino-neto <laudelino_sc@hotmail.com>
2026-04-09 23:55:27 -0300

    Configuracao do CORS

--- src/main/java/br/com/larcash/config/AppConfig.java
+++ src/main/java/br/com/larcash/config/AppConfig.java
@@ -1,9 +1,13 @@
 package br.com.larcash.config;
 
+import java.util.Arrays;
 import java.util.TimeZone;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 
@@ -21,5 +25,21 @@ public class AppConfig {
     public Hibernate5JakartaModule jsonHibernate5Module() {
         return new Hibernate5JakartaModule();
     }
+	
+	@Configuration
+	public class CorsConfig {
+	    @Bean
+	    public CorsConfigurationSource corsConfigurationSource() {
+	    	CorsConfiguration corsConfiguration = new CorsConfiguration();
+		    corsConfiguration.applyPermitDefaultValues(); 
+		    corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+		    corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+		    corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		    corsConfiguration.setExposedHeaders(Arrays.asList("*"));
+		    UrlBasedCorsConfigurationSource ccs = new UrlBasedCorsConfigurationSource();
+		    ccs.registerCorsConfiguration("/**", corsConfiguration);
+		    return ccs;
+	    }
+	}
 
 }
