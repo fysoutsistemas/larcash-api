c32f4de76038d3f68c656964fd2c674d38c887fd
laudelino-neto <laudelino_sc@hotmail.com>
2026-04-10 00:16:16 -0300

    Novo ajuste

--- src/main/java/br/com/larcash/config/AppConfig.java
+++ src/main/java/br/com/larcash/config/AppConfig.java
@@ -1,15 +1,9 @@
 package br.com.larcash.config;
 
-import java.util.Arrays;
 import java.util.TimeZone;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 
@@ -28,20 +22,4 @@ public class AppConfig {
         return new Hibernate5JakartaModule();
     }
 	
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .exposedHeaders("*")
-                        .maxAge(3600);
-            }
-        };
-    }	
-
 }
